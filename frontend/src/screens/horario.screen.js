0dcb3469ad536eadfef37f6b1b3336240e67e8bb
cristofer <cristofer.mellado2101@alumnos.ubiobio.cl>
2025-02-28 22:01:11 -0300

    fix styles on horario

--- frontend/src/screens/horario.screen.js
+++ frontend/src/screens/horario.screen.js
@@ -7,6 +7,7 @@ import {
   FlatList,
   View,
   Modal,
+  ActivityIndicator
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import horarioService from '../services/horario.service';
