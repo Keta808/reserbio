46927fcd4e7fd912522ea432addfc718c79f6447
cristofer <cristofer.mellado2101@alumnos.ubiobio.cl>
2025-03-03 22:42:24 -0300

    fix

--- backend/src/routes/servicio.routes.js
+++ backend/src/routes/servicio.routes.js
@@ -9,17 +9,19 @@ import { isAdmin, isCliente, isTrabajador } from "../middlewares/authorization.m
 
 import authentificationMiddleware from "../middlewares/authentication.middleware.js"; 
 import verificarAdminMicroempresa from "../middlewares/verificarAdminM.middleware.js";
+
+
 const router = Router(); 
 
 router.use(authentificationMiddleware); 
 
 router.get("/", servicioController.getServicios); 
-router.post("/", verificarAdminMicroempresa.verificarAdminMicroempresa, servicioController.createServicio);
-router.delete("/:id", verificarAdminMicroempresa.verificarAdminMicroempresa, servicioController.deleteServicio);
-router.put("/:id", verificarAdminMicroempresa.verificarAdminMicroempresa, servicioController.updateServicio);
+router.post("/", verificarAdminMicroempresa, servicioController.createServicio);
+router.delete("/:id", verificarAdminMicroempresa, servicioController.deleteServicio);
+router.put("/:id", verificarAdminMicroempresa, servicioController.updateServicio);
 router.get("/servicio/:id", servicioController.getServicioById); 
 router.get("/servicios/:id", servicioController.getServiciosByMicroempresaId);
-router.post("/servicio/:id", verificarAdminMicroempresa.verificarAdminMicroempresa, servicioController.configurarPorcentajeAbono);
+router.post("/servicio/:id", verificarAdminMicroempresa, servicioController.configurarPorcentajeAbono);
 router.post("/servicios/:id", servicioController.calcularMontoAbono);
 
 router.get("/servicio/:id/microempresa", servicioController.getMicroempresaIdByServicioId);
